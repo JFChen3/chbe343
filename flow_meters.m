b4a599c45059410d8b445fdc034cd1f56227ea63
ethanjahn <ethanjahn@gmail.com>
2016-03-23 22:33:06 -0500

    Update flow_meters.m

--- flow_meters.m
+++ flow_meters.m
@@ -87,3 +87,96 @@ function [ordered] = reorder(vector)
 
 ordered = [vector(2:3) vector(1) vector(4:end)];
 end
+
+rho = 998.02;
+mu = 1.002e-3;
+dia = .24; %m
+
+
+flow_rates = [
+    0.4468    0.5110    0.2636    0.4589    0.5801
+    0.6563    0.7129    0.3883    0.6365    0.8142
+    0.5250    0.5804    0.3160    0.5232    0.7212
+    0.2973    0.3975    0.2099    0.3813    0.4947
+    0.2000    0.2713    0.1392    0.2453    0.3498
+    0.1163    0.1451    0.0693    0.1388    0.1749
+    0.0645    0.0631    0.0313    0.0566    0.1010
+    0.0317    0.0315    0.0140    0.0317    0.0553   
+    ];
+
+
+flow_rates = sort(flow_rates,1);
+flow_rates = flow_rates .* 10^-3;
+
+velocities = flow_rates ./ ((dia/2)^2 * pi);
+
+re = rho.*velocities.*dia./mu;
+
+v_turb = mu*4000/(rho*dia);
+v_lam = mu*2000/(rho*dia);
+
+q_turb = v_turb * (dia/2)^2 * pi;
+q_lam = v_lam * (dia/2)^2 * pi;
+
+coriolis = flow_rates(:,2);
+stopwatch = flow_rates(:,1);
+orifice = flow_rates(:,3);
+venturi = flow_rates(:,4);
+pitot = flow_rates(:,5);
+
+coriolis_v = velocities(:,2);
+stopwatch_v = velocities(:,1);
+orifice_v = velocities(:,3);
+venturi_v = velocities(:,4);
+pitot_v = velocities(:,5);
+
+coriolis_error = (coriolis - stopwatch) ./ stopwatch;
+orifice_error = (orifice - stopwatch) ./ stopwatch;
+venturi_error = (venturi - stopwatch) ./ stopwatch;
+pitot_error = (pitot - stopwatch) ./ stopwatch;
+
+stopwatch_error_c = (stopwatch - coriolis) ./ coriolis;
+orifice_error_c = (orifice - coriolis) ./ coriolis;
+venturi_error_c = (venturi - coriolis) ./ coriolis;
+pitot_error_c = (pitot - coriolis) ./ coriolis;
+
+mean_coriolis_error = mean(coriolis_error);
+mean_orifice_error = mean(orifice_error);
+mean_venturi_error = mean(venturi_error);
+mean_pitot_error = mean(pitot_error);
+
+%disp(mean_coriolis_error)
+%disp(mean_orifice_error)
+%disp(mean_venturi_error)
+%disp(mean_pitot_error)
+
+% pitot plot
+y_range = -80:80;
+
+hold on
+%plot(stopwatch,coriolis_error.*100)
+plot(stopwatch,orifice_error.*100,'--r')
+plot(stopwatch,venturi_error.*100,'-xg')
+%plot(stopwatch,pitot_error.*100,'-sk')
+plot(ones(1,numel(y_range)).*q_turb,y_range,'-.m')
+plot(ones(1,numel(y_range)).*q_lam,y_range,'-.m')
+%title('Error as a Function of Flow Rate')
+xlabel('Flow Rate (m^3/s)')
+ylabel('Percent Error Compared to Stopwatch Measurement')
+%legend('Coriolis Meter','Orifice Plate','Venturi Meter','Pitot Tube')
+legend('Orifice Plate','Venturi Meter','Bounds of Transition Regime','location','northwest')
+
+figure()
+% pitot plot
+hold on
+%plot(coriolis,stopwatch_error_c.*100)
+plot(coriolis,orifice_error_c.*100,'--r')
+plot(coriolis,venturi_error_c.*100,'-xg')
+%plot(coriolis,pitot_error_c.*100,'-sk')
+plot(ones(1,numel(y_range)).*q_turb,y_range,'-.m')
+plot(ones(1,numel(y_range)).*q_lam,y_range,'-.m')
+%title('Error as a Function of Flow Rate')
+xlabel('Flow Rate (m^3/s)')
+ylabel('Percent Error Compared to Coriolis Measurement')
+%legend('Stopwatch Measurement','Orifice Plate','Venturi Meter','Pitot Tube')
+legend('Orifice Plate','Venturi Meter','Bounds of Transition Regime','location','northwest')
