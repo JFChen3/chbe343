f9fc3919e2f952631971b4bdb42a01ec5c6959b1
Jiming Chen <jc75@rice.edu>
2016-04-17 17:17:26 -0500

    add air conditioner lab

---
+++ air_conditioner.m
@@ -0,0 +1,78 @@
+function air_conditioner
+
+C_power = 3.6; %kWh/rev
+B_meter = 14.4; %kWh/rev
+pulse_scale = 0.03204; %Convert refrigerant flow to gph (who the fuck uses gallons)
+P_atm = 14.7;
+
+% TC 1 – Intake air temperature
+% TC 2 – Intake wet bulb temperature
+% TC 3 – Intake dry bulb temperature
+% TC 4 – Condensate exiting evaporator
+% TC 5 – High side refrigerant (liquid entering expansion valve from compressor)
+% TC 6 – Low side refrigerant (vapor leaving the evaporator coils & returning to compressor)
+% TC 7 – Exit wet bulb temperature (between evaporator and blower)
+% TC 8 – Exit dry bulb temperature (between evaporator and blower)
+% TC 9 – Exit temperature (at the blower)
+
+%%Low flow rate data
+T = [72.6, 73.3, 66.4, 61.9, 79.2, 55.7, 47.6, 50.6, 52.5]; %F
+H_air = [20.5, 19.7];
+H_refrig = [];
+comp_time = 8.89; %s
+blow_time = 231.5; %s
+power = calc_power([C_power, B_meter],[comp_time,blow_time]);
+ref_flow = 758*0.03204; %Refrigerant flow
+Anemometer = 1.22;
+humidity_in = 0.638;
+humidity_out = 0.973;
+pressure_1 = 65 + P_atm;
+pressure_2 = 195 + P_atm;
+condense_flow = 362/378; %mL/s
+
+%%High flow rate data
+T = [72.8, 73.3, 66.3, 63.9, 80.8, 61.3, 56.4, 57.8, 59.8]; %F
+H_air = [20.5, 14.7];
+comp_time = 8.53; %s
+blow_time = 144.5; %s
+power = calc_power([C_power, B_meter],[comp_time,blow_time]);
+ref_flow = 843*0.03204; %Refrigerant flow
+Anemometer = 1.44;
+humidity_in = 0.650;
+humidity_out = 0.955;
+pressure_1 = 73 + P_atm;
+pressure_2 = 203 + P_atm;
+condense_flow = 229/270;
+
+%%Expansion valve data
+%P1 = 50
+P2_50 = [55,50:-1:32]; %psig
+flow_rate_50 = [0,0,0.8,1.2,1.7,2.2,2.6,3.0,3.2,3.5,3.8,3.9,4.0,4.2,4.3,4.3,4.4,4.4,4.4,4.4]; %scfm
+
+%P1 = 15
+P2_15 = 50:-1:34;
+flow_rate_15 = [0,0,0,0.8,0.9,1.0,1.2,1.2,1.4,1.4,1.5,1.6,1.6,1.6,1.7,1.7,1.7];
+
+plot(P2_50,flow_rate_50,'.-',P2_15,flow_rate_15,'o-')
+xlabel('Flow Rate (scfm)')
+ylabel('P_2 (psig)')
+legend('P_1 = 50 psig','P_2 = 15 psig')
+
+end
+
+function [power] = calc_power(power_per_rev, rev_time)
+%Compute total power input, power_per_rev in kWh/rev, rev_time in s
+
+power = sum(power_per_rev./(rev_time./3600));
+
+end
+
+function ac_performance()
+
+end
+
+function [velocity] = anemometer_cal(pulse)
+
+velocity = 12.02*pulse^2 - 13.627*pulse + 3.7286;
+
+end
