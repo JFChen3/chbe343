9dc7c27921a239487a453587748e6a37e3701c4a
Jiming Chen <jc75@rice.edu>
2016-04-17 18:04:27 -0500

    added CoP

--- air_conditioner.m
+++ air_conditioner.m
@@ -17,12 +17,17 @@ P_atm = 14.7;
 
 %%Low flow rate data
 T = [72.6, 73.3, 66.4, 61.9, 79.2, 55.7, 47.6, 50.6, 52.5]; %F
-H_air = [20.5, 19.7];
-H_refrig = [];
+H_air = [20.5, 19.7]; %Enthalpy of air going into and out of evaporator, Btu/lb-dry
+H_refrig = [20.045, 35.771]; %Enthalpy of refrigerant going into and out of evaporator, kJ/mol
 comp_time = 8.89; %s
 blow_time = 231.5; %s
-power = calc_power([C_power, B_meter],[comp_time,blow_time]);
-ref_flow = 758*0.03204; %Refrigerant flow
+power = calc_power([C_power, B_meter],[comp_time,blow_time]); %kW
+ref_flow = 758; %Refrigerant pulse
+
+%Coefficient of performance
+[CoP] = coeff_performance(ref_flow, H_refrig, power);
+fprintf('Coefficient of performance, high flow rate: %f\n',CoP)
+
 Anemometer = 1.22;
 humidity_in = 0.638;
 humidity_out = 0.973;
@@ -32,11 +37,17 @@ condense_flow = 362/378; %mL/s
 
 %%High flow rate data
 T = [72.8, 73.3, 66.3, 63.9, 80.8, 61.3, 56.4, 57.8, 59.8]; %F
-H_air = [20.5, 14.7];
+H_air = [20.5, 21.5];
+H_refrig = [20.129, 35.879];
 comp_time = 8.53; %s
 blow_time = 144.5; %s
-power = calc_power([C_power, B_meter],[comp_time,blow_time]);
-ref_flow = 843*0.03204; %Refrigerant flow
+power = calc_power([C_power, B_meter],[comp_time,blow_time]); %kW
+ref_flow = 843; %Refrigerant pulse
+
+%Coefficient of performance
+[CoP] = coeff_performance(ref_flow, H_refrig, power);
+fprintf('Coefficient of performance, low flow rate: %f\n',CoP)
+
 Anemometer = 1.44;
 humidity_in = 0.650;
 humidity_out = 0.955;
@@ -57,6 +68,20 @@ plot(P2_50,flow_rate_50,'.-',P2_15,flow_rate_15,'o-')
 xlabel('Flow Rate (scfm)')
 ylabel('P_2 (psig)')
 legend('P_1 = 50 psig','P_2 = 15 psig')
+
+end
+
+function [CoP] = coeff_performance(refrig_pulse, H_refrig, power)
+
+refrig_flow = molar_flow_rate_refrig(refrig_pulse); %mol/s
+Qc = refrig_flow*(H_refrig(2) - H_refrig(1)); %kJ/s
+CoP = Qc/power; %dimensionless
+
+end
+
+function [flow_rate] = molar_flow_rate_refrig(pulse)
+
+flow_rate = (pulse*0.03204*0.000001052*1184)/0.08647; %mol/s
 
 end
 
