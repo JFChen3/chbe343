0226b0f25f3c0eb9dbd403501daed2584c824a4e
ethanjahn <ethanjahn@gmail.com>
2016-04-19 21:45:44 -0500

    Update air_conditioner.m
    Changed the expansion valve plot and added initial analysis of the degrees of superheat

--- air_conditioner.m
+++ air_conditioner.m
@@ -5,15 +5,15 @@ B_meter = 14.4; %kWh/rev
 pulse_scale = 0.03204; %Convert refrigerant flow to gph (who the fuck uses gallons)
 P_atm = 14.7;
 
-% TC 1 – Intake air temperature
-% TC 2 – Intake wet bulb temperature
-% TC 3 – Intake dry bulb temperature
-% TC 4 – Condensate exiting evaporator
-% TC 5 – High side refrigerant (liquid entering expansion valve from compressor)
-% TC 6 – Low side refrigerant (vapor leaving the evaporator coils & returning to compressor)
-% TC 7 – Exit wet bulb temperature (between evaporator and blower)
-% TC 8 – Exit dry bulb temperature (between evaporator and blower)
-% TC 9 – Exit temperature (at the blower)
+% TC 1 â€“ Intake air temperature
+% TC 2 â€“ Intake wet bulb temperature
+% TC 3 â€“ Intake dry bulb temperature
+% TC 4 â€“ Condensate exiting evaporator
+% TC 5 â€“ High side refrigerant (liquid entering expansion valve from compressor)
+% TC 6 â€“ Low side refrigerant (vapor leaving the evaporator coils & returning to compressor)
+% TC 7 â€“ Exit wet bulb temperature (between evaporator and blower)
+% TC 8 â€“ Exit dry bulb temperature (between evaporator and blower)
+% TC 9 â€“ Exit temperature (at the blower)
 
 %%Low flow rate data
 T = [72.6, 73.3, 66.4, 61.9, 79.2, 55.7, 47.6, 50.6, 52.5]; %F
@@ -58,16 +58,35 @@ condense_flow = 229/270;
 %%Expansion valve data
 %P1 = 50
 P2_50 = [55,50:-1:32]; %psig
+P2_50a = P2_50 + 14.7; % psia
 flow_rate_50 = [0,0,0.8,1.2,1.7,2.2,2.6,3.0,3.2,3.5,3.8,3.9,4.0,4.2,4.3,4.3,4.4,4.4,4.4,4.4]; %scfm
 
 %P1 = 15
 P2_15 = 50:-1:34;
+P2_15a = P2_15 + 14.7; % psia
 flow_rate_15 = [0,0,0,0.8,0.9,1.0,1.2,1.2,1.4,1.4,1.5,1.6,1.6,1.6,1.7,1.7,1.7];
 
+psat = r22_sat_pressure(273.15);
+title('Flow Rate versus Pressure Differential')
+fa_50 = psat - P2_50a;
+fa_15 = psat - P2_15a;
+prop_fa_50 = fa_50 / psat;
+prop_fa_15 = fa_15 / psat;
+temp_50 = 273.15 * prop_fa_50;
+temp_15 = 273.15 * prop_fa_15;
+xlabel('Flow Rate (SCFM)')
+ylabel('Psat - P2')
+
+figure(1)
+hold on
+plot(flow_rate_50,fa_50)
+plot(flow_rate_15,fa_15)
+
+figure(2)
 plot(P2_50,flow_rate_50,'.-',P2_15,flow_rate_15,'o-')
-xlabel('Flow Rate (scfm)')
-ylabel('P_2 (psig)')
-legend('P_1 = 50 psig','P_2 = 15 psig')
+ylabel('Flow Rate (scfm)')
+xlabel('P_2 (psig)')
+legend('P_1 = 50 psig','P_1 = 15 psig')
 
 end
 
@@ -96,7 +115,23 @@ function ac_performance()
 
 end
 
+function out = r22_sat_pressure(T)
+% Antoine equation (temp in K)
+% Constants found on Nist Webbook based on Stull 1947
+A = 4.36567;
+B = 947.577;
+C = -14.964;
+logP = A - (B/(T+C));
+out = 10^(logP); % bar
+out = out * 14.5038; % bar to psi
+
+end
+
 function [velocity] = anemometer_cal(pulse)
+
+velocity = 12.02*pulse^2 - 13.627*pulse + 3.7286;
+
+end
 
 velocity = 12.02*pulse^2 - 13.627*pulse + 3.7286;
 
