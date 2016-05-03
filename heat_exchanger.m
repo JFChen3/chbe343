e7e8ff233a0b78d33050de1b215efa8827721826
Jiming Chen <jc75@rice.edu>
2016-05-03 13:40:38 -0500

    changed heat excganger type

--- heat_exchanger.m
+++ heat_exchanger.m
@@ -7,36 +7,40 @@ function heat_exchanger
 %Thermal data for CO2, IL: viscosity, thermal conductivity, heat capacity
 %Conductivity of tube material
 
-%%%DATA, listed in [IL, CO2] (Check values for all of these, should be
-%%%averages across temp range)
-mass_flow = [59600/3600, 596000*0.65/3600]; %kg/s
-density = [1115, 120]; %kg/m^3
-mu = [5.5e-2 ,3.1e-5]; %Dynamic viscosity, Pa*s
+%%%DATA, listed in [IL, CO2] (Will be different for each stream integration stage)
+mass_flow = [1649.52, 596000/3600]; %kg/s
+density = [1115, 29.18]; %kg/m^3
+mu = [5.5e-2,3.44e-5]; %Dynamic viscosity, Pa*s
 nu = mu./density; %Kinematic viscosity
-k = [0.16, 0.05]; %Thermal conductivity, W/(m*K)
-Cp = [1200, 1181]; %Heat capacity, J/(kg*K)
+k = [0.16, 0.056]; %Thermal conductivity, W/(m*K)
+Cp = [1800, 1170]; %Heat capacity, J/(kg*K)
 
-L_range = linspace(1,10,20)';
-eff_NTU_calc(mass_flow, density, mu, nu, k, Cp, L_range, [70+273, 600])
+L_range = linspace(1,10)';
+
+q_total = 1649.52*1200*91;
+q_stage = q_total/4;
+
+dT = q_stage./(mass_flow.*Cp)
+eff_NTU_calc(mass_flow, density, mu, nu, k, Cp, L_range, [70+273, 634], q_stage)
 
 end
 
-function eff_NTU_calc(mass_flow, density, mu, nu, k, Cp, L, Ti)
+function [L_exchanger] = eff_NTU_calc(mass_flow, density, mu, nu, k, Cp, L, Ti, q_stage)
 %Eff-NTU calculation to get q vs L
 
 %%%Tube dimensions
 %Shell
-D_large = 5.0;
+D_large = 4.0;
 A_large = pi*(D_large/2)^2;
 %Inner tube
-Di = 0.04;
-Do = 0.0415;
+Di = 0.03;
+Do = 0.0315;
 A_small = pi*(Di/2)^2;
 A_tube = pi*(Do/2)^2;
-N_tubes = floor(A_large/(A_tube*4)); %Number of tubes that fit
+N_tubes = floor(A_large/(A_tube*2)); %Number of tubes that fit
 % N_tubes = 1000;
 fprintf('Number of tubes: %.0f\n', N_tubes)
-kt = 18; %Stainless steel tube
+kt = 385; %Stainless steel tube
 
 %Convert mass flow rates to volumetric flow rates and velocities
 volume_flow = mass_flow ./density;
@@ -45,6 +49,11 @@ v = volume_flow ./ ([(A_small*N_tubes), (A_large-N_tubes*A_tube)]); %Assume IL o
 Re = reynolds(nu, v, L); %Reynolds number
 
 Pr = prandtl(Cp, mu, k); %Prandtl number
+
+%%CO2 pressure drop, check to see if need to include
+f = 0.184*(Re(2))^-0.2;
+dP = (f.*density(2).*(v(2))^2.*L)./(2*D_large);
+
 
 hi = (k(1)*nusselt(Re(:,1),Pr(1)))./L; %Convection coefficient
 ho = (k(2)*nusselt(Re(:,2),Pr(2)))./L; 
@@ -55,16 +64,19 @@ UA = heat_transfer_coeff(Di, Do, L, kt, hi, ho); %Total heat transfer coeffs
 NTU = UA./(min(Cp.*mass_flow)); %Number of transfer units
 Cr = min(Cp.*mass_flow)/max(Cp.*mass_flow); %Heat capacity ratio
 
-% eps_parallel = eff_parallel(NTU, Cr); %effectiveness, parallel flow
-% eps_counter = eff_counter(NTU, Cr); %effectiveness, counter flow
-eps_shell = N_tubes*eff_shell(NTU, Cr);
-
+eps = eff_shell(NTU, Cr);
+n=2;
+x = (1-eps.*Cr)./(1-eps).^n;
+eps_shell = (x - 1)./(x - Cr); %Multi shell pass
 %Heat duty
-q_shell = eps_shell.*qmax;
+q_shell = N_tubes*eps_shell.*qmax;
 
 plot(L, q_shell)
 xlabel('Exchanger Tube Length')
 ylabel('Heat Duty')
+
+L_sufficient = L(q_shell > q_stage);
+L_exchanger = L_sufficient(1);
 
 end
 
@@ -107,15 +119,3 @@ B = 1-exp(-NTU.*x);
 epsilon = 2*(1+Cr+x.*(A./B)).^-1;
 
 end
-
-% function [epsilon] = eff_parallel(NTU, Cr)
-% 
-% epsilon = (1-exp(-NTU.*(1+Cr)))./(1+Cr);
-% 
-% end
-% 
-% function [epsilon] = eff_counter(NTU, Cr)
-% 
-% epsilon = (1-exp(-NTU.*(1-Cr)))./(1-Cr.*exp(-NTU.*(1-Cr)));
-% 
-% end
